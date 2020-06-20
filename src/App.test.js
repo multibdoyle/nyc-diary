58bef3b5608782a6e6f13227d3714b4cd18186db
Brendan <bdoyle.core@gmail.com>
2020-06-20 19:15:44 -0400

    Added Google Analytics

--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,40 @@ test('renders learn react link', () => {
 
 
 // Code for elements of Navbar
+
+
+
+<div className='social-media' >
+<NavItem  style={{
+                margin: 1,
+                top:0,
+                right: 0,
+         
+                left:400  ,
+             position: 'relative'
+             
+            }}>
+<Nav.Link href='https://github.com/multibdoyle/'><img src={require("./github_logo.png")} class="img-thumbnail" alt='logo' width='25px' float='right'></img></Nav.Link>
+</NavItem>
+<NavItem style={{
+                margin: 1,
+                top:-1.5,
+                right: 0,
+                left: 400  ,
+             position: 'relative'
+             
+            }} >
+<Nav.Link href='https://www.linkedin.com/in/brendan-doyle-403ba028' ><img src={require("./linkedin_logo.png")} class="img-thumbnail"alt='linkedinlogo' width='25px' float='right'></img></Nav.Link>
+</NavItem>
+<NavItem style={{
+                margin: 1,
+                top:-1.5,
+                right: 0,
+         
+                left:400  ,
+             position: 'relative'
+             
+            }}>
+<Nav.Link href='https://www.instagram.com/multibdoyle/'><img src={require("./instagram_logo.png")} class="img-thumbnail" alt='instagramlogo' width='25px' float='right'></img></Nav.Link>
+</NavItem>
+</div>

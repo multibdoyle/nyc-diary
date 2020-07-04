53c9e24c26c4d14573b980a65909e228b8d4d918
Brendan <bdoyle.core@gmail.com>
2020-07-04 15:38:11 -0400

    Adding Ghost Kitchens Post

--- src/Components/PageBody/PageBody.jsx
+++ src/Components/PageBody/PageBody.jsx
@@ -16,7 +16,19 @@ class PageBody extends React.Component
 <h1 align='center'></h1>
 
     <div>
-    
+    <a><Link to='/ghost-kitchens'><h3 style={{fontFamily:'Archer', fontSize:25}}>Enter the Ghost Kitchens</h3></Link></a> 
+   <h5> Saturday, July 4th, 2020</h5>
+    <body>
+      <filter id='ghosts'>
+       <img id='swedish-chef' src='https://live.staticflickr.com/84/235702343_2d7c71a86f_b.jpg' 
+class="img-thumbnail" alt='intro' 
+/* eslint no-restricted-globals:0 */
+       style={{ width:'17%',height: '17%', position:'relative',  borderRadius:5,contrast:'200%' }}></img>
+       </filter>
+       </body>
+     <a> <div style={{fontFamily:'Archer',fontSize: 25, position:'relative',top:-25}}><Link to='/ghost-kitchens'>Wherein I explore the otherwordly realm of virtual restaurants.</Link></div></a>
+   </div>
+
      <a><Link to='/intro'><h3 style={{fontFamily:'Archer', fontSize:25}}>Why Am I Here?</h3></Link></a> 
     <h5> Sunday, June 14th, 2020</h5>
      <body>
@@ -27,6 +39,10 @@ class PageBody extends React.Component
       <a> <div style={{fontFamily:'Archer',fontSize: 25, position:'relative',top:-25}}><Link to='/intro'>It's a new dawn, it's a new day, it's a new light.</Link></div></a>
     </div>
 
+
+    <div>
+    
+   
       <Router>
       </Router>
 

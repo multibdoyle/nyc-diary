5b4cdac7f07f6aef393750c3acbed0a2a5a9499b
Brendan <bdoyle.core@gmail.com>
2020-08-23 18:33:03 -0400

    testing again

--- src/Components/Posts/BodybuildingCookies.jsx
+++ src/Components/Posts/BodybuildingCookies.jsx
@@ -21,19 +21,7 @@ class BodybuildingCookies extends React.Component
          }}>
      <body >
     <div align='center'> 
- <InstagramEmbed
-  url='https://www.instagram.com/p/CEHhdVEHExU/'
-  maxWidth={320}
-  hideCaption={true}
-  containerTagName='div'
-  protocol=''
-  injectScript
-  onLoading={() => {}}
-  onSuccess={() => {}}
-  onAfterRender={() => {}}
-  onFailure={() => {}}
 
-   ></InstagramEmbed>
 </div>
 
      <h6 style={{fontSize:14}}> Saturday, August 22nd, 2020</h6>

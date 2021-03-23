2782ef7d927a941d9debdcd90377591a498e72ea
Brendan <bdoyle.core@gmail.com>
2021-03-22 20:13:34 -0400

    data

--- src/Components/Posts/Butterfunk.jsx
+++ src/Components/Posts/Butterfunk.jsx
@@ -29,13 +29,13 @@ class ButterfunkPost extends React.Component
 <p> <a href='https://www.butterfunkbiscuit.com/' target='blank' style={{color:'blue', fontsize:16}}> Butterfunk Biscuit Co. </a>is the brainchild of Chef Chris Scott, who uses his family's fourth-generation biscuit recipe across a wide swath of regional touchpoints from New York City and the American South. The Smoked Salmon Biscuit, Avocado Toast Biscuit and Crispy Fried Chicken Biscuit are all top-billers on the menu. </p>
 
     <p>
-    After some deliberation, I went all in on the Chopped Cheese Biscuit. A chopped cheese, for those unaware, is the uptown New York City bodega order <i>du jour</i>*, akin to a bacon-egg-and-cheese in Midtown, or a hummus wrap and SmartWater in Tribeca. 
+Choosing between different comfort food dishes, for me, is like picking a winner in a cute baby contest, so I had some difficulty honing in on one particular option from Butterfunk's menu.  After some deliberation, I decided on the Chopped Cheese Biscuit. A chopped cheese, for those unaware, is the uptown New York City bodega order <i>du jour</i>*, akin to a bacon-egg-and-cheese in Midtown, or a hummus wrap and SmartWater in Tribeca. 
 </p>
 <p>I finished off my order with the "Berry Berry Good Shortcake" — can’t skip dessert! — and hopped on a Zoom catch-up with a friend while I waited. 
 
 </p>
     
-<p>When the food arrived, I plopped my bag of goodies on my bed and pulled out the first item at the top of the bag, which turned out to be the shortcake. 
+<p>When my food arrived, I plopped the bag of goodies on my bed and pulled out the first item at the top of the bag, which turned out to be the shortcake. 
 
 </p>
 <p>Right away, I sensed something was wrong. My berry-shortcake medley looked more like croutons, crème fraîche, and yellow cherry tomatoes with a sprig of parsley. 
@@ -62,7 +62,7 @@ class ButterfunkPost extends React.Component
 <p> I had thought shortcake was basically sponge cake; pardon me for being honest about my ignorance. In either case, Butterfunk's shortcake dessert succumbed to a case of too toasty biscuits; I prefer my cakes to caress, not crunch. I'd give this dish a 3 out of 5.  </p>
 
 
-<p>The Chopped Cheese Biscuit gets a 4 out of 5. The ground beef was hearty, the cheese was melty, the biscuit was, um, toasted. Maybe a little too toasted, which keeps the sandwich from a solid 4. Still, I’d absolutely order another Butterfunk biscuit sandwich. 
+<p>The Chopped Cheese Biscuit gets a 4 out of 5. The ground beef was hearty, the cheese was melty, the biscuit was, um, toasted. Maybe a little too toasted, which keeps the sandwich from a higher score. Still, I’d absolutely order another Butterfunk biscuit sandwich. 
 </p>
 <p>So overall, a 3.5 out of 5 for Butterfunk Biscuit Co. Perhaps their score was a little handicapped by my lack of knowledge about shortcake, but you know, they've pretty much got the Southern biscuit market cornered in New York; they deserve to be held to a high standard.
 </p>
